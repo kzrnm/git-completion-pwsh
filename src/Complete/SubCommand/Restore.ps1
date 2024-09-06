f3b6092e653a8bb9df8b63d3c1e06dfd5004e494
kzrnm <gengesa@gmail.com>
2024-09-07 00:30:24 +0900

    Restore

---
+++ src/Complete/SubCommand/Restore.ps1
@@ -0,0 +1,76 @@
+using namespace System.Collections.Generic;
+using namespace System.Management.Automation;
+
+function Complete-GitSubCommand-restore {
+    [CmdletBinding(PositionalBinding = $false)]
+    [OutputType([CompletionResult[]])]
+    param(
+        [CommandLineContext]
+        [Parameter(Position = 0, Mandatory)]$Context
+    )
+
+    [string] $Current = $Context.CurrentWord()
+    if (!$Context.HasDoubledash()) {
+        if ($Current -eq '-') {
+            return Get-GitShortOptions $Context.command
+        }
+
+
+        $conflictCandidates = 'diff3', 'merge', 'zdiff3'
+        $prevCandidates = switch -CaseSensitive ($Context.PreviousWord()) {
+            { $_ -cin @('-s', '--source') } {
+                gitCompleteRefs -Current $Current
+                return
+            }
+            '--conflict' { $conflictCandidates }
+        }
+
+        if ($prevCandidates) {
+            $prevCandidates | completeList -Current $Current -ResultType ParameterValue
+            return
+        }
+
+        if ($Current -cmatch '(--[^=]+)=(.*)') {
+            $key = $Matches[1]
+            $value = $Matches[2]
+            $candidates = switch -CaseSensitive ($key) {
+                '--source' {
+                    gitCompleteRefs -Current $value -Prefix "$key="
+                    return
+                }
+                '--conflict' { $conflictCandidates }
+            }
+
+            if ($candidates) {
+                $candidates | completeList -Current $Current -Prefix "$key=" -ResultType ParameterValue -RemovePrefix
+                return
+            }
+        }
+
+        if ($Current.StartsWith('--')) {
+            gitCompleteResolveBuiltins $Context.command -Current $Current
+            return
+        }
+    }
+
+    if (gitPseudorefExists HEAD) {
+        $completeOpt = @('--modified')
+        $skipOptions = [System.Collections.Generic.List[string]]::new()
+        foreach ($opt in (gitResolveBuiltins $Context.command -All)) {
+            if ($opt.EndsWith('=')) {
+                $skipOptions.Add($opt)
+            }
+        }
+        $UsedPaths = [System.Collections.Generic.List[string]]::new($Context.Words.Length)
+        for ($i = $Context.commandIndex + 1; $i -lt $Context.Words.Length; $i++) {
+            if ($i -eq $Context.CurrentIndex) { continue }
+            $w = $Context.Words[$i]
+            if ($skipOptions.Contains($w)) { $i++ }
+            elseif (!$w.StartsWith('-') -or ($i -gt $Context.DoubledashIndex)) {
+                $UsedPaths.Add($w)
+            }
+        }
+
+        gitCompleteIndexFile -Current $Current -Options $completeOpt -Exclude $UsedPaths -LeadingDash:($Context.HasDoubledash())
+    }
+}
---
+++ tests/SubCommand/Restore.Tests.ps1
@@ -0,0 +1,233 @@
+﻿using namespace System.Collections.Generic;
+using namespace System.IO;
+
+. "$($script:RepoRoot = $PSScriptRoot.Substring(0, $PSScriptRoot.LastIndexOf('tests')).Replace('\', '/'))testtools/TestInitialize.ps1"
+
+Describe (Get-Item $PSCommandPath).BaseName.Replace('.Tests', '') {
+    BeforeAll {
+        Set-Variable Command ((Get-Item $PSCommandPath).BaseName.Replace('.Tests', '') | ConvertTo-KebabCase)
+        Initialize-Home
+
+        mkdir ($rootPath = "$TestDrive/gitRoot")
+        mkdir ($remotePath = "$TestDrive/gitRemote")
+        Initialize-FilesRepo $rootPath $remotePath
+        Push-Location $rootPath
+    }
+
+    AfterAll {
+        Restore-Home
+        Pop-Location
+    }
+
+    Describe 'DoubleDash' {
+        Describe 'InRight' {
+            It '<Left>(cursor)<Right>' -ForEach @(
+                @{
+                    Left     = '--quiet';
+                    Right    = ' --';
+                    Expected = '--quiet' | ConvertTo-Completion -ResultType ParameterName -ToolTip 'suppress progress reporting'
+                },
+                @{
+                    Left     = '--quiet';
+                    Right    = ' -- --quiet';
+                    Expected = '--quiet' | ConvertTo-Completion -ResultType ParameterName -ToolTip 'suppress progress reporting'
+                }
+            ) {
+                "git $Command $Left" | Complete-FromLine -Right $Right | Should -BeCompletion $Expected
+            }
+        }
+
+        It '<Line>' -ForEach @(
+            @{
+                Line     = 'src -- -';
+                Expected = @()
+            },
+            @{
+                Line     = 'src -- --';
+                Expected = @()
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+
+    It 'ShortOptions' {
+        $Expected = @{
+            ListItemText = '-2';
+            ToolTip      = "checkout our version for unmerged files";
+        },
+        @{
+            ListItemText = '-3';
+            ToolTip      = "checkout their version for unmerged files";
+        },
+        @{
+            ListItemText = '-m';
+            ToolTip      = "perform a 3-way merge with the new branch";
+        },
+        @{
+            ListItemText = '-p';
+            ToolTip      = "select hunks interactively";
+        },
+        @{
+            ListItemText = '-q';
+            ToolTip      = "suppress progress reporting";
+        },
+        @{
+            ListItemText = '-s';
+            ToolTip      = "which tree-ish to checkout from";
+        },
+        @{
+            ListItemText = '-S';
+            ToolTip      = "restore the index";
+        },
+        @{
+            ListItemText = '-W';
+            ToolTip      = "restore the working tree (default)";
+        },
+        @{
+            ListItemText = '-h';
+            ToolTip      = "show help";
+        } | ConvertTo-Completion -ResultType ParameterName
+        "git $Command -" | Complete-FromLine | Should -BeCompletion $expected
+    }
+
+    Describe 'Options' {
+        It '<Line>' -ForEach @(
+            @{
+                Line     = '--o';
+                Expected = @{
+                    ListItemText = '--overlay';
+                    ToolTip      = "use overlay mode";
+                },
+                @{
+                    ListItemText = '--ours';
+                    ToolTip      = "checkout our version for unmerged files";
+                } | ConvertTo-Completion -ResultType ParameterName
+            },
+            @{
+                Line     = '--no-q';
+                Expected = @{
+                    ListItemText = '--no-quiet';
+                    ToolTip      = "[NO] suppress progress reporting";
+                } | ConvertTo-Completion -ResultType ParameterName
+            },
+            @{
+                Line     = '--no';
+                Expected = @{
+                    ListItemText = '--no-source';
+                    ToolTip      = "[NO] which tree-ish to checkout from"
+                },
+                @{
+                    CompletionText = '--no-';
+                    ListItemText   = '--no-...';
+                    ResultType     = 'Text'
+                } | ConvertTo-Completion -ResultType ParameterName
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+
+    Describe 'OptionValue' {
+        It '<Line>' -ForEach @(
+            @{
+                Line     = '-s ';
+                Expected = 
+                'HEAD',
+                'FETCH_HEAD',
+                'main',
+                'grm/main',
+                'ordinary/main',
+                'origin/main',
+                'initial' | ConvertTo-Completion -ResultType ParameterValue
+            },
+            @{
+                Line     = '-s ^o';
+                Expected = 
+                '^ordinary/main',
+                '^origin/main' | ConvertTo-Completion -ResultType ParameterValue
+            },
+            @{
+                Line     = '--source ';
+                Expected = 
+                'HEAD',
+                'FETCH_HEAD',
+                'main',
+                'grm/main',
+                'ordinary/main',
+                'origin/main',
+                'initial' | ConvertTo-Completion -ResultType ParameterValue
+            },
+            @{
+                Line     = '--source ^m';
+                Expected = 
+                '^main' | ConvertTo-Completion -ResultType ParameterValue
+            },
+            @{
+                Line     = '--source=';
+                Expected =
+                'HEAD',
+                'FETCH_HEAD',
+                'main',
+                'grm/main',
+                'ordinary/main',
+                'origin/main',
+                'initial' | ForEach-Object {
+                    "$_" | ConvertTo-Completion -ResultType ParameterValue -CompletionText "--source=$_"
+                }
+            },
+            @{
+                Line     = '--source=m';
+                Expected =
+                'main' | ForEach-Object {
+                    "$_" | ConvertTo-Completion -ResultType ParameterValue -CompletionText "--source=$_"
+                }
+            },
+            @{
+                Line     = '--conflict ';
+                Expected = 
+                'diff3', 'merge', 'zdiff3' | ConvertTo-Completion -ResultType ParameterValue
+            },
+            @{
+                Line     = '--conflict z';
+                Expected = 
+                'zdiff3' | ConvertTo-Completion -ResultType ParameterValue
+            },
+            @{
+                Line     = '--conflict=';
+                Expected =
+                'diff3', 'merge', 'zdiff3' | ForEach-Object {
+                    "$_" | ConvertTo-Completion -ResultType ParameterValue -CompletionText "--conflict=$_"
+                }
+            },
+            @{
+                Line     = '--conflict=z';
+                Expected =
+                'zdiff3' | ForEach-Object {
+                    "$_" | ConvertTo-Completion -ResultType ParameterValue -CompletionText "--conflict=$_"
+                }
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+
+    Describe 'File' {
+        It '<Line>' -ForEach @(
+            @{
+                Line     = ' ';
+                Expected = @{
+                    CompletionText = "Aquarion`` Evol/Evol";
+                    ListItemText   = "Aquarion Evol/Evol"
+                },
+                'Dr.Wily' | ConvertTo-Completion -ResultType ProviderItem
+            },
+            @{
+                Line     = 'D';
+                Expected = 'Dr.Wily' | ConvertTo-Completion -ResultType ProviderItem
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+}
