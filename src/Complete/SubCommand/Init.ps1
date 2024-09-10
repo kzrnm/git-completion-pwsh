48f261404b1b57f43e1a920fe452aa657b8e7ebb
kzrnm <gengesa@gmail.com>
2024-09-10 19:39:10 +0900

    init

---
+++ src/Complete/SubCommand/Init.ps1
@@ -0,0 +1,34 @@
+using namespace System.Management.Automation;
+
+function Complete-GitSubCommand-init {
+    [CmdletBinding(PositionalBinding = $false)]
+    [OutputType([CompletionResult[]])]
+    param(
+        [CommandLineContext]
+        [Parameter(Position = 0, Mandatory)]$Context
+    )
+
+    [string] $Current = $Context.CurrentWord()
+    if (!$Context.HasDoubledash()) {
+        $shortOpts = Get-GitShortOptions $Context.command -Current $Current
+        if ($shortOpts) { return $shortOpts }
+
+        if ($Current -cmatch '(--[^=]+)=(.*)') {
+            $key = $Matches[1]
+            $value = $Matches[2]
+            $candidates = switch -CaseSensitive ($key) {
+                '--shared' { 'false', 'true', 'umask', 'group', 'all', 'world', 'everybody' }
+            }
+
+            if ($candidates) {
+                $candidates | completeList -Current $value -Prefix "$key=" -ResultType ParameterValue
+                return
+            }
+        }
+
+        if ($Current.StartsWith('--')) {
+            gitCompleteResolveBuiltins $Context.command -Current $Current
+            return
+        }
+    }
+}
---
+++ tests/SubCommand/Init.Tests.ps1
@@ -0,0 +1,126 @@
+using namespace System.Collections.Generic;
+using namespace System.IO;
+
+. "$($script:RepoRoot = $PSScriptRoot.Substring(0, $PSScriptRoot.LastIndexOf('tests')).Replace('\', '/'))testtools/TestInitialize.ps1"
+
+Describe (Get-Item $PSCommandPath).BaseName.Replace('.Tests', '') {
+    BeforeAll {
+        Set-Variable Command ((Get-Item $PSCommandPath).BaseName.Replace('.Tests', '') | ConvertTo-KebabCase)
+        Initialize-Home
+    }
+
+    AfterAll {
+        Restore-Home
+        Pop-Location
+    }
+
+    Describe 'DoubleDash' {
+        Describe 'InRight' {
+            It '<Left>(cursor)<Right>' -ForEach @(
+                @{
+                    Left     = '--quiet';
+                    Right    = ' --';
+                    Expected = '--quiet' | ConvertTo-Completion -ResultType ParameterName -ToolTip 'be quiet'
+                },
+                @{
+                    Left     = '--quiet';
+                    Right    = ' -- --all';
+                    Expected = '--quiet' | ConvertTo-Completion -ResultType ParameterName -ToolTip 'be quiet'
+                }
+            ) {
+                "git $Command $Left" | Complete-FromLine -Right $Right | Should -BeCompletion $Expected
+            }
+        }
+
+        It '<Line>' -ForEach @(
+            @{
+                Line     = 'src -- -';
+                Expected = @()
+            },
+            @{
+                Line     = 'src -- --';
+                Expected = @()
+            },
+            @{
+                Line     = 'src -- ';
+                Expected = @()
+            },
+            @{
+                Line     = '-- ';
+                Expected = @()
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+
+    It 'ShortOptions' {
+        $Expected = @{
+            ListItemText = '-b';
+            ToolTip      = "override the name of the initial branch";
+        },
+        @{
+            ListItemText = '-q';
+            ToolTip      = "be quiet";
+        },
+        @{
+            ListItemText = '-h';
+            ToolTip      = "show help";
+        } | ConvertTo-Completion -ResultType ParameterName
+        "git $Command -" | Complete-FromLine | Should -BeCompletion $expected
+    }
+
+    Describe 'Options' {
+        It '<Line>' -ForEach @(
+            @{
+                Line     = '--b';
+                Expected = @{
+                    ListItemText = '--bare';
+                    ToolTip      = "create a bare repository";
+                } | ConvertTo-Completion -ResultType ParameterName
+            },
+            @{
+                Line     = '--no-b';
+                Expected = @{
+                    ListItemText = '--no-bare';
+                    ToolTip      = "[NO] create a bare repository";
+                } | ConvertTo-Completion -ResultType ParameterName
+            },
+            @{
+                Line     = '--no';
+                Expected = @{
+                    ListItemText = '--no-template';
+                    ToolTip      = "[NO] directory from which templates will be used";
+                },
+                @{
+                    CompletionText = '--no-';
+                    ListItemText   = '--no-...';
+                    ResultType     = 'Text';
+                } | ConvertTo-Completion -ResultType ParameterName
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+
+    Describe 'OptionValue' {
+        It '<Line>' -ForEach @(
+            @{
+                Line     = '--shared=';
+                Expected = 'false',
+                'true',
+                'umask',
+                'group',
+                'all',
+                'world',
+                'everybody' | ConvertTo-Completion -ResultType ParameterValue -CompletionText { "--shared=$_" }
+            },
+            @{
+                Line     = '--shared=u';
+                Expected = 'umask' | ConvertTo-Completion -ResultType ParameterValue -CompletionText { "--shared=$_" }
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+}
