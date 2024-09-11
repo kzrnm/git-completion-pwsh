4f7c321ff6b40ca277af2e107a98e1314f83f588
kzrnm <gengesa@gmail.com>
2024-09-11 22:24:45 +0900

    svn

---
+++ src/Complete/SubCommand/Svn.ps1
@@ -0,0 +1,56 @@
+using namespace System.Management.Automation;
+
+function Complete-GitSubCommand-svn {
+    [CmdletBinding(PositionalBinding = $false)]
+    [OutputType([CompletionResult[]])]
+    param(
+        [CommandLineContext]
+        [Parameter(Position = 0, Mandatory)]$Context
+    )
+
+    [string] $Current = $Context.CurrentWord()
+    $Subcommand = $Context.SubcommandWithoutGlobalOption()
+    if (!$Subcommand) {
+        'init', 'fetch', 'clone', 'rebase', 'dcommit', 'log',
+        'find-rev', 'set-tree', 'commit-diff', 'info',
+        'create-ignore', 'propget', 'proplist', 'show-ignore',
+        'show-externals', 'branch', 'tag', 'blame',
+        'migrate', 'mkdirs', 'reset', 'gc' | completeList -Current $Current -ResultType ParameterName
+        return
+    }
+
+    if (!$Context.HasDoubledash() -and $Current.StartsWith('--')) {
+        $remoteOpts = '--username=', '--config-dir=', '--no-auth-cache'
+        $fcOpts = @('--follow-parent', '--authors-file=', '--repack=', '--no-metadata', '--use-svm-props', '--use-svnsync-props', '--log-window-size=', '--no-checkout', '--quiet', '--repack-flags', '--use-log-author', '--localtime', '--add-author-from', '--recursive', '--ignore-paths=', '--include-paths=') + $remoteOpts
+        $initOpts = @('--template=', '--shared=', '--trunk=', '--tags=', '--branches=', '--stdlayout', '--minimize-url', '--no-metadata', '--use-svm-props', '--use-svnsync-props', '--rewrite-root=', '--prefix=') + $remoteOpts
+        $cmtOpts = '--edit', '--rmdir', '--find-copies-harder', '--copy-similarity='
+        
+        $Candidates = switch ($Subcommand) {
+            'fetch' { @('--revision=', '--fetch-all') + $fcOpts }
+            'clone' { @('--revision=') + $fcOpts + $initOpts }
+            'init' { $initOpts }
+            'dcommit' {
+                @('--merge', '--strategy=', '--verbose', '--dry-run', '--fetch-all', '--no-rebase', '--commit-url', '--revision', '--interactive') + $cmtOpts + $fcOpts
+            }
+            'set-tree' {
+                @('--stdin') + $cmtOpts + $fcOpts
+            }
+            'rebase' {
+                @('--merge', '--verbose', '--strategy=', '--local', '--fetch-all', '--dry-run') + $fcOpts
+            }
+            'commit-diff' {
+                @('--message=', '--file=', '--revision=') + $cmtOpts
+            }
+            'log' { '--limit=', '--revision=', '--verbose', '--incremental', '--oneline', '--show-commit', '--non-recursive', '--authors-file=', '--color' }
+            'info' { '--url' }
+            'branch' { '--dry-run', '--message', '--tag' }
+            'tag' { '--dry-run', '--message' }
+            'blame' { '--git-format' }
+            'migrate' { '--config-dir=', '--ignore-paths=', '--minimize', '--no-auth-cache', '--username=' }
+            'reset' { '--revision=', '--parent' }
+            { $_ -cin 'create-ignore', 'propget', 'proplist', 'show-ignore', 'show-externals', 'mkdirs' } { '--revision=' }
+        }
+
+        $Candidates | completeList -Current $Current -ResultType ParameterName
+    }
+}
---
+++ tests/SubCommand/Svn.Tests.ps1
@@ -0,0 +1,546 @@
+using namespace System.Collections.Generic;
+using namespace System.IO;
+
+. "$($script:RepoRoot = $PSScriptRoot.Substring(0, $PSScriptRoot.LastIndexOf('tests')).Replace('\', '/'))testtools/TestInitialize.ps1"
+
+Describe (Get-Item $PSCommandPath).BaseName.Replace('.Tests', '') {
+    BeforeAll {
+        Set-Variable Command ((Get-Item $PSCommandPath).BaseName.Replace('.Tests', '') | ConvertTo-KebabCase)
+    }
+
+    Describe 'Subcommands' {
+        It '<Line>' -ForEach @(
+            @{
+                Line     = ""
+                Expected =
+                'init',
+                'fetch',
+                'clone',
+                'rebase',
+                'dcommit',
+                'log',
+                'find-rev',
+                'set-tree',
+                'commit-diff',
+                'info',
+                'create-ignore',
+                'propget',
+                'proplist',
+                'show-ignore',
+                'show-externals',
+                'branch',
+                'tag',
+                'blame',
+                'migrate',
+                'mkdirs',
+                'reset',
+                'gc' | ConvertTo-Completion -ResultType ParameterName
+            },
+            @{
+                Line = "re"
+                Expected =
+                'rebase', 'reset' | ConvertTo-Completion -ResultType ParameterName
+            }
+        ) {
+            "git $Command $Line" | Complete-FromLine | Should -BeCompletion $expected
+        }
+    }
+
+    Describe 'fetch' {
+        BeforeAll {
+            Set-Variable Subcommand 'fetch'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--revision=',
+                    '--fetch-all',
+                    '--follow-parent',
+                    '--authors-file=',
+                    '--repack=',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--log-window-size=',
+                    '--no-checkout',
+                    '--quiet',
+                    '--repack-flags',
+                    '--use-log-author',
+                    '--localtime',
+                    '--add-author-from',
+                    '--recursive',
+                    '--ignore-paths=',
+                    '--include-paths=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--q';
+                    Expected = '--quiet' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+
+    Describe 'clone' {
+        BeforeAll {
+            Set-Variable Subcommand 'clone'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--revision=',
+                    '--follow-parent',
+                    '--authors-file=',
+                    '--repack=',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--log-window-size=',
+                    '--no-checkout',
+                    '--quiet',
+                    '--repack-flags',
+                    '--use-log-author',
+                    '--localtime',
+                    '--add-author-from',
+                    '--recursive',
+                    '--ignore-paths=',
+                    '--include-paths=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache',
+                    '--template=',
+                    '--shared=',
+                    '--trunk=',
+                    '--tags=',
+                    '--branches=',
+                    '--stdlayout',
+                    '--minimize-url',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--rewrite-root=',
+                    '--prefix=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--q';
+                    Expected =
+                    '--quiet' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'init' {
+        BeforeAll {
+            Set-Variable Subcommand 'init'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--template=',
+                    '--shared=',
+                    '--trunk=',
+                    '--tags=',
+                    '--branches=',
+                    '--stdlayout',
+                    '--minimize-url',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--rewrite-root=',
+                    '--prefix=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--p';
+                    Expected =
+                    '--prefix=' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'dcommit' {
+        BeforeAll {
+            Set-Variable Subcommand 'dcommit'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--merge',
+                    '--strategy=',
+                    '--verbose',
+                    '--dry-run',
+                    '--fetch-all',
+                    '--no-rebase',
+                    '--commit-url',
+                    '--revision',
+                    '--interactive',
+                    '--edit',
+                    '--rmdir',
+                    '--find-copies-harder',
+                    '--copy-similarity=',
+                    '--follow-parent',
+                    '--authors-file=',
+                    '--repack=',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--log-window-size=',
+                    '--no-checkout',
+                    '--quiet',
+                    '--repack-flags',
+                    '--use-log-author',
+                    '--localtime',
+                    '--add-author-from',
+                    '--recursive',
+                    '--ignore-paths=',
+                    '--include-paths=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--q';
+                    Expected =
+                    '--quiet' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'set-tree' {
+        BeforeAll {
+            Set-Variable Subcommand 'set-tree'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--stdin',
+                    '--edit',
+                    '--rmdir',
+                    '--find-copies-harder',
+                    '--copy-similarity=',
+                    '--follow-parent',
+                    '--authors-file=',
+                    '--repack=',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--log-window-size=',
+                    '--no-checkout',
+                    '--quiet',
+                    '--repack-flags',
+                    '--use-log-author',
+                    '--localtime',
+                    '--add-author-from',
+                    '--recursive',
+                    '--ignore-paths=',
+                    '--include-paths=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--q';
+                    Expected =
+                    '--quiet' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'rebase' {
+        BeforeAll {
+            Set-Variable Subcommand 'rebase'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--merge',
+                    '--verbose',
+                    '--strategy=',
+                    '--local',
+                    '--fetch-all',
+                    '--dry-run',
+                    '--follow-parent',
+                    '--authors-file=',
+                    '--repack=',
+                    '--no-metadata',
+                    '--use-svm-props',
+                    '--use-svnsync-props',
+                    '--log-window-size=',
+                    '--no-checkout',
+                    '--quiet',
+                    '--repack-flags',
+                    '--use-log-author',
+                    '--localtime',
+                    '--add-author-from',
+                    '--recursive',
+                    '--ignore-paths=',
+                    '--include-paths=',
+                    '--username=',
+                    '--config-dir=',
+                    '--no-auth-cache' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--q';
+                    Expected =
+                    '--quiet' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'commit-diff' {
+        BeforeAll {
+            Set-Variable Subcommand 'commit-diff'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--message=',
+                    '--file=',
+                    '--revision=',
+                    '--edit',
+                    '--rmdir',
+                    '--find-copies-harder',
+                    '--copy-similarity=' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--e';
+                    Expected =
+                    '--edit' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'log' {
+        BeforeAll {
+            Set-Variable Subcommand 'log'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--limit=',
+                    '--revision=',
+                    '--verbose',
+                    '--incremental',
+                    '--oneline',
+                    '--show-commit',
+                    '--non-recursive',
+                    '--authors-file=',
+                    '--color' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--v';
+                    Expected =
+                    '--verbose' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'info' {
+        BeforeAll {
+            Set-Variable Subcommand 'info'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--url' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--u';
+                    Expected =
+                    '--url' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'branch' {
+        BeforeAll {
+            Set-Variable Subcommand 'branch'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--dry-run',
+                    '--message',
+                    '--tag' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--t';
+                    Expected =
+                    '--tag' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'tag' {
+        BeforeAll {
+            Set-Variable Subcommand 'tag'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--dry-run',
+                    '--message' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--m';
+                    Expected =
+                    '--message' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'blame' {
+        BeforeAll {
+            Set-Variable Subcommand 'blame'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--git-format' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--g';
+                    Expected =
+                    '--git-format' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'migrate' {
+        BeforeAll {
+            Set-Variable Subcommand 'migrate'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--config-dir=',
+                    '--ignore-paths=',
+                    '--minimize',
+                    '--no-auth-cache',
+                    '--username=' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--u';
+                    Expected =
+                    '--username=' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe 'reset' {
+        BeforeAll {
+            Set-Variable Subcommand 'reset'
+        }
+        Describe 'Options' {
+            It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected =
+                    '--revision=',
+                    '--parent' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--p';
+                    Expected =
+                    '--parent' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+
+    Describe '<Subcommand>' -ForEach @(
+        'create-ignore',
+        'propget',
+        'proplist',
+        'show-ignore',
+        'show-externals',
+        'mkdirs' | ForEach-Object { @{Subcommand = $_ } }) {
+        Describe 'Options' { It '<Line>' -ForEach @(
+                @{
+                    Line     = '--';
+                    Expected = '--revision=' | ConvertTo-Completion -ResultType ParameterName
+                },
+                @{
+                    Line     = '--rev';
+                    Expected = '--revision=' | ConvertTo-Completion -ResultType ParameterName
+                }
+            ) {
+                "git $Command $Subcommand $Line" | Complete-FromLine | Should -BeCompletion $expected
+            }
+        }
+    }
+}
