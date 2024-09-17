ba71b0936a83cf44ccb8072ff9b464b6239b5e5f
kzrnm <gengesa@gmail.com>
2024-09-18 08:47:26 +0900

    v1.0.3

--- tools/dist.ps1
+++ tools/dist.ps1
@@ -16,7 +16,7 @@ $Module = $env:Module
 
 mkdir $module
 
-Copy-Item * -Recurse -Destination $module -Exclude $module, src, tests, tools, .github, .gitignore, .vscode
+Copy-Item * -Recurse -Destination $module -Exclude $module, src, tests, tools, testtools, .github, .gitignore, .vscode
 Copy-Item src/* -Recurse -Destination $module
 
 (Get-Content "./src/$module.psd1" -Raw).Replace('blob/naub', "blob/$TagName") | Out-File -Encoding utf8NoBOM -FilePath "./$module/$module.psd1"
